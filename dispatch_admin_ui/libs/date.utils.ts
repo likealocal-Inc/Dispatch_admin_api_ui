c95a56a35f63d4a32aa358ee96063314c954af32
seungjinhan <hanblues@gmail.com>
2023-05-22 15:28:13 +0900

    stringToDate 함수에서 인자를 정수처리

--- dispatch_admin_ui/libs/date.utils.ts
+++ dispatch_admin_ui/libs/date.utils.ts
@@ -17,7 +17,7 @@ export enum DateAddType {
  */
 export const DateUtils = {
   stringToDate(str: string) {
-    var d = new Date(str * 1000), // Convert the passed timestamp to milliseconds
+    var d = new Date(parseInt(str) * 1000), // Convert the passed timestamp to milliseconds
       yyyy = d.getFullYear(),
       mm = ("0" + (d.getMonth() + 1)).slice(-2), // Months are zero based. Add leading 0.
       dd = ("0" + d.getDate()).slice(-2), // Add leading 0.
