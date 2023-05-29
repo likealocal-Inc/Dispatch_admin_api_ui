ec72d8261eef408036b6b86b279d64f16f4b1dd0
seungjinhan <hanblues@gmail.com>
2023-05-29 15:41:20 +0900

    code

--- dispatch_admin_ui/libs/client/constants.ts
+++ dispatch_admin_ui/libs/client/constants.ts
@@ -25,7 +25,7 @@ export const PageURLs = {
   ROOT: "/",
   LOGIN: "/auth/login",
   JOIN: "/auth/join",
-  MAIN: "/admin/dashboard",
+  MAIN: "/admin/dispatch",
 };
 
 export interface APIURLType {
